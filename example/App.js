ef47c2763459d986b471ba77f51efaf46b4bc6bf
FreakyCoder <kurayogun@gmail.com>
2019-10-06 22:28:00 +0300

    BottomSearchBar has a TextInput now and a completely new example is integrated

--- example/App.js
+++ example/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <BottomSearchBar />
+        <BottomSearchBar onChangeText={text => alert(text)} />
       </View>
     );
   }
