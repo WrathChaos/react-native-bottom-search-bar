f2c1efc31383e16e2bdf4e8eaeb3da57b57b1917
FreakyCoder <kurayogun@gmail.com>
2019-12-26 11:32:36 +0300

    SearchBox: TextInput is fully customizable now

--- example/App.js
+++ example/App.js
@@ -7,7 +7,7 @@ export default class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <BottomSearchBar disableTextInput onChangeText={text => alert(text)} />
+        <BottomSearchBar onChangeText={text => console.log(text)} />
       </View>
     );
   }
