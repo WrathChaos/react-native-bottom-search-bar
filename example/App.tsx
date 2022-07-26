332c2e6267d240a6b78e3f43c3afe99bec5a095c
FreakyCoder <kurayogun@gmail.com>
2022-07-26 23:12:42 +0300

    chore: version 1

--- example/App.tsx
+++ example/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import BottomSearchBar from './lib/BottomSearchBar';
+import BottomSearchBar from './build/dist/BottomSearchBar';
 
 export default class App extends React.Component {
   render() {
